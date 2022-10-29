cd99a1105199ea70b99abd4116b73b30a0a57c19
Tobi <adewumitobi20@gmail.com>
2022-10-30 00:01:23 +0200

    a

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -9,10 +9,10 @@ export default function Footer() {
             <div className='container'>
                 <div className='content'>
                     <div className='zuri'>
-                        <img className= 'img1' src={footer1} alt='' />   
+                        <img className= 'img1' src={footer1} alt='zuri' />   
                     </div>
                     <div className='footer_text'>HNG Internship 9 Frontend Task</div>
-                    <div className='g'><img src={I4G} alt='' /> </div>
+                    <div className='g'><img src={I4G} alt='i4g' /> </div>
                 </div>
         
             </div>
